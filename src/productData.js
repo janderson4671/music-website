62dd54687c917412c2a8088f69a1667cb0679454
Nathan Craddock <nzcraddock@gmail.com>
2020-10-30 03:01:32 +0000

    Blurbs

--- src/productData.js
+++ src/productData.js
@@ -2,63 +2,63 @@ let Products = [{
     id: 1,
     name: "Clarinet",
     imageurl: "/images/clarinet.jpg",
-    blurb: "It sounds good :)",
+    blurb: "In the woodwind family",
     infoData: "The clarinet is a family of woodwind instruments. It has a single-reed mouthpiece, a straight, cylindrical tube with an almost cylindrical bore, and a flared bell. A person who plays a clarinet is called a clarinetist (sometimes spelled clarinettist)."
 },
 {
     id: 2,
     name: "Guitar",
     imageurl: "/images/guitar.jpg",
-    blurb: "It sounds good :)",
+    blurb: "A six-stringed instrument",
     infoData: "The guitar is a fretted musical instrument that usually has six strings.[1] It is typically played with both hands by strumming or plucking the strings with either a guitar pick or the fingers/fingernails of one hand, while simultaneously fretting (pressing the strings against the frets) with the fingers of the other hand. The sound of the vibrating strings is projected either acoustically, by means of the hollow chamber of the guitar (for an acoustic guitar), or through an electrical amplifier and a speaker."
 },
 {
     id: 3,
     name: "Harp",
     imageurl: "/images/harp.jpg",
-    blurb: "It sounds good :)",
+    blurb: "A sideways piano?",
     infoData: "The harp is a stringed musical instrument that has a number of individual strings running at an angle to its soundboard; the strings are plucked with the fingers."
 },
 {
     id: 2,
     name: "Ocarina",
     imageurl: "/images/ocarina.jpg",
-    blurb: "It sounds good :)",
+    blurb: "Link plays one of these",
     infoData: "The ocarina is an ancient wind musical instrument—a type of vessel flute.[1] Variations exist, but a typical ocarina is an enclosed space with four to twelve finger holes and a mouthpiece that projects from the body. It is traditionally made from clay or ceramic, but other materials are also used—such as plastic, wood, glass, metal, or bone."
 },
 {
     id: 2,
     name: "Piano",
     imageurl: "/images/piano.jpg",
-    blurb: "It sounds good :)",
+    blurb: "The \"soft-loud\"",
     infoData: "The piano is an acoustic, stringed musical instrument invented in Italy by Bartolomeo Cristofori around the year 1700 (the exact year is uncertain), in which the strings are struck by wooden hammers that are coated with a softer material (modern hammers are covered with dense wool felt; some early pianos used leather). It is played using a keyboard, which is a row of keys (small levers) that the performer presses down or strikes with the fingers and thumbs of both hands to cause the hammers to strike the strings."
 },
 {
     id: 2,
     name: "Trumpet",
     imageurl: "/images/trumpet.jpg",
-    blurb: "It sounds good :)",
+    blurb: "A brass instrument",
     infoData: "The trumpet is a brass instrument commonly used in classical and jazz ensembles. The trumpet group ranges from the piccolo trumpet with the highest register in the brass family, to the bass trumpet, which is pitched one octave below the standard B♭ or C Trumpet."
 },
 {
     id: 2,
     name: "Ukulele",
     imageurl: "/images/uke.jpg",
-    blurb: "It sounds good :)",
+    blurb: "Sounds like Hawaii",
     infoData: "The ukulele originated in the 19th century as a Hawaiian adaptation of the Portuguese machete (cavaquinho),[4] a small guitar-like instrument, which was introduced to Hawaii by Portuguese immigrants, mainly from Madeira and the Azores. It gained great popularity elsewhere in the United States during the early 20th century and from there spread internationally."
 },
 {
     id: 2,
     name: "Violin",
     imageurl: "/images/violin.jpg",
-    blurb: "It sounds good :)",
+    blurb: "A stringed instrument played with a bow",
     infoData: "The violin, sometimes known as a fiddle, is a wooden chordophone (string instrument) in the violin family. Most violins have a hollow wooden body. It is the smallest and thus highest-pitched instrument (soprano) in the family in regular use.[a] The violin typically has four strings, usually tuned in perfect fifths with notes G3, D4, A4, E5, and is most commonly played by drawing a bow across its strings. It can also be played by plucking the strings with the fingers (pizzicato) and, in specialized cases, by striking the strings with the wooden side of the bow (col legno)."
 },
 {
     id: 2,
     name: "Xylophone",
     imageurl: "/images/xylophone.jpg",
-    blurb: "It sounds good :)",
+    blurb: "Drum piano?",
     infoData: "The xylophone is a musical instrument in the percussion family that consists of wooden bars struck by mallets. Like the glockenspiel, the xylophone essentially consists of a set of tuned keys arranged in the fashion of the keyboard of a piano. Each bar is an idiophone tuned to a pitch of a musical scale, whether pentatonic or heptatonic in the case of many African and Asian instruments, diatonic in many western children's instruments, or chromatic for orchestral use."
 },
 
