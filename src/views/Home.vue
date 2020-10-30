9130202bd639bbc9b57a6dfd07192f27a7ddf222
Jason Anderson <anderson.jason.az@gmail.com>
2020-10-29 18:00:40 -0600

    Fixed broken link in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <h3>It's good, but not too good eh!</h3>
 
     <p>
-      Please take a look at our <a href="/products">Products</a> to see
+      Please take a look at our <router-link to="/products">Products</router-link> to see
       what sort of musically-oriented wares we offer.
     </p>
     <img src="/images/guy.jpg" alt="Hi There:) If you found this...something went wrong">
